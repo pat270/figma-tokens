4e61661d8ae12e392af8c2497494c14c61817521
MightyDevKnight <takao.kamio@brandcode.dev>
2022-07-13 15:17:42 +0900

    send activeTheme, availableTheme, usedTokenSet data

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback } from 'react';
 import axios from 'axios';
+import { useSelector } from 'react-redux';
 import Box from './Box';
 import { Tabs } from '@/constants/Tabs';
 import Stack from './Stack';
@@ -10,24 +11,32 @@ import useMinimizeWindow from './useMinimizeWindow';
 import IconButton from './IconButton';
 import { IconFolder } from '@/icons';
 import useTokens from '@/app/store/useTokens';
+import { activeThemeSelector, themeOptionsSelector, usedTokenSetSelector } from '@/selectors';
 
 const Navbar: React.FC = () => {
   const { handleResize } = useMinimizeWindow();
   const { getFormattedTokens } = useTokens();
+  const activeTheme = useSelector(activeThemeSelector);
+  const availableThemes = useSelector(themeOptionsSelector);
+  const usedTokenSet = useSelector(usedTokenSetSelector);
+
   const handleOpenTokenFlowApp = useCallback(async () => {
     const tokens = getFormattedTokens({
       includeAllTokens: true, includeParent: false, expandTypography: false, expandShadow: false,
     });
-    const data = JSON.stringify(tokens, null, 2);
+    const tokenData = JSON.stringify(tokens, null, 2);
     const response = await axios({
       method: 'post',
       url: 'http://localhost:3000/api/tokens',
       data: {
-        data,
+        tokenData,
+        activeTheme,
+        availableThemes,
+        usedTokenSet,
       },
     });
     if (response.status === 200) window.open(`http://localhost:3000?id=${response.data.result}`);
-  }, [getFormattedTokens]);
+  }, [activeTheme, availableThemes, getFormattedTokens, usedTokenSet]);
 
   return (
     <Box
